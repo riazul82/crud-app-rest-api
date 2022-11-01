4ad30c4bc3acb538aa27f22e8bf08b74819f8223
riazul islam <riazulislam5280@gmail.com>
2022-11-02 02:33:38 +0600

    user controller updated!

--- controllers/user.control.js
+++ controllers/user.control.js
@@ -4,7 +4,6 @@ const User = require('../models/users.model');
 const getAllUser = async (req, res) => {
     try {
         const users = await User.find();
-        console.log(users);
         res.status(200).json({ users });
     } catch (err) {
         res.status(500).json({ message: err.message });
@@ -14,12 +13,15 @@ const getAllUser = async (req, res) => {
 // create user
 const createUser = async (req, res) => {
     try {
-        const user = await User.findOne({ email: req.body.email });
+        let user = await User.findOne({ email: req.body.email });
         if (user) {
             return res.status(403).json({ message: 'user already exist!' });
         }
+        user = await User.findOne({username: req.body.username});
+        if (user) {
+            return res.status(403).json({ message: 'username is already taken!' });
+        }
         const newUser = await User.create(req.body);
-        console.log(newUser);
         res.status(201).json({ message: 'user created!' });
     } catch (err) {
         res.status(500).json({ message: err.message });
@@ -29,17 +31,23 @@ const createUser = async (req, res) => {
 // update user
 const updateUser = async (req, res) => {
     try {
-        const user = await User.findOne({
-            _id: req.params.id
-        });
+        let existUser = await User.findOne({ _id: {$ne: req.params.id},  email: req.body.email });
+        if (existUser) {
+            return res.status(403).json({ message: 'user already exist!' });
+        }
+        existUser = await User.findOne({ _id: {$ne: req.params.id},  username: req.body.username });
+        if (existUser) {
+            return res.status(403).json({ message: 'username is already taken!' });
+        }
+        const user = await User.findOne({ _id: req.params.id });
         if (req.body.username) {
-            user.username = req.body.username
+            user.username = req.body.username;
         }
         if (req.body.email) {
-            user.email = req.body.email
+            user.email = req.body.email;
         }
         if (req.body.phone) {
-            user.phone = req.body.phone
+            user.phone = req.body.phone;
         }
         await user.save();
         res.status(200).json({ message: 'user updated!' });
